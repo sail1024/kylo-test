accbab36c77c3f3b4ba040247b987020a4ff1edd
李雪垠 <358020853@qq.com>
2019-11-21 19:11:53 +0800

    liquibase

---
+++ stella-test-liquibase/src/main/java/com/stella/test/liquibase/LiquibaseApplication.java
@@ -0,0 +1,18 @@
+package com.stella.test.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * bootstrap
+ *
+ * @author sail
+ * @date 17:35 2019-11-21.
+ * @since 1.0
+ */
+@SpringBootApplication
+public class LiquibaseApplication {
+    public static void main(String[] args){
+        SpringApplication.run(LiquibaseApplication.class, args);
+    }
+}
